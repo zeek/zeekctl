5d3c15d9524ec708a30dd6b46aa4cfdb8a253f9f
Robin Sommer <robin@icir.org>
2011-07-30 18:58:14 -0700

    Little tweaks.
    Fill in real mesage later ...

--- BroControl/config.py
+++ BroControl/config.py
@@ -347,7 +347,7 @@ class Configuration:
                 config[key] = val
 
         except IOError, e:
-            util.warn("warning: cannot read '%s' (this is ok on first run)" % file)
+            util.warn("cannot read '%s' (this is ok on first run)" % file)
 
         return config
 
