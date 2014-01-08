d47f27e461413a27f2a25ff4a953fad5adaf4615
Daniel Thayer <dnthayer@illinois.edu>
2014-01-08 14:43:33 -0600

    Change default PF_RING cluster type to 4-tuple

--- BroControl/options.py
+++ BroControl/options.py
@@ -96,7 +96,7 @@ options = [
 
     Option("PFRINGClusterID", "@PF_RING_CLUSTER_ID@", "int", Option.USER, False,
            "If PF_RING flow-based load balancing is desired, this is where the PF_RING cluster id is defined. The default value is configuration-dependent and determined automatically by CMake at configure-time based upon whether PF_RING's enhanced libpcap is available.  Bro must be linked with PF_RING's libpcap wrapper for this option to work."),
-    Option("PFRINGClusterType", "6-tuple", "string", Option.USER, False,
+    Option("PFRINGClusterType", "4-tuple", "string", Option.USER, False,
            "If PF_RING flow-based load balancing is desired, this is where the PF_RING cluster type is defined.  Allowed values are: 2-tuple, 4-tuple, 5-tuple, tcp-5-tuple, 6-tuple, or round-robin.  Bro must be linked with PF_RING's libpcap wrapper and PFRINGClusterID must be non-zero for this option to work."),
 
     Option("CFlowAddress", "", "string", Option.USER, False,
