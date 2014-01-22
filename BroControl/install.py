3992022db32d1f1869ab2076692152c629b7f887
Daniel Thayer <dnthayer@illinois.edu>
2014-01-21 22:50:21 -0600

    Fix bug with timemachineport broctl option
    Removed extra "/tcp" from the timemachine port (it's already included
    in the string value).  Also improved formatting of cluster-layout.bro
    for readability.

--- BroControl/install.py
+++ BroControl/install.py
@@ -260,11 +260,11 @@ def makeLayout(path, silent=False):
         # Workers definition
         for w in workers:
             p = w.count % len(proxies)
-            print >>out, "\t[\"%s\"] = [$node_type=Cluster::WORKER, $ip=%s, $zone_id=\"%s\", $p=%s/tcp, $interface=\"%s\", $manager=\"%s\", $proxy=\"%s\"]," % (w.name, util.formatBroAddr(w.addr), w.zone_id, nextPort(w), w.interface, manager.name, proxies[p].name),
+            print >>out, "\t[\"%s\"] = [$node_type=Cluster::WORKER, $ip=%s, $zone_id=\"%s\", $p=%s/tcp, $interface=\"%s\", $manager=\"%s\", $proxy=\"%s\"]," % (w.name, util.formatBroAddr(w.addr), w.zone_id, nextPort(w), w.interface, manager.name, proxies[p].name)
 
         # Activate time-machine support if configured.
         if config.Config.timemachinehost:
-            print >>out, "[\"time-machine\"] = [$node_type=Cluster::TIME_MACHINE, $ip=%s, $p=%s/tcp]," % (config.Config.timemachinehost, config.Config.timemachineport),
+            print >>out, "\t[\"time-machine\"] = [$node_type=Cluster::TIME_MACHINE, $ip=%s, $p=%s]," % (config.Config.timemachinehost, config.Config.timemachineport)
 
         print >>out, "};"
 
