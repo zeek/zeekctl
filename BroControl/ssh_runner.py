5a1e0dfd26535fa6a1b5879889dfe9c7e8893646
Daniel Thayer <dnthayer@illinois.edu>
2014-09-24 12:22:38 -0500

    Move the ssh_runner muxer code to a function

--- BroControl/ssh_runner.py
+++ BroControl/ssh_runner.py
@@ -3,73 +3,80 @@ import collections
 import json
 import subprocess
 import select
+import textwrap
 import time
 
-muxer=r"""
-import json
-import os
-import sys
-import subprocess
-import signal
-import select
+def get_muxer():
+    muxer = r"""
+    import json
+    import os
+    import sys
+    import subprocess
+    import signal
+    import select
 
-TIMEOUT=120
+    TIMEOUT=120
 
-def w(s):
-    sys.stdout.write(json.dumps(s) + "\n")
-    sys.stdout.flush()
+    def w(s):
+        sys.stdout.write(json.dumps(s) + "\n")
+        sys.stdout.flush()
 
-def exec_commands(cmds):
-    procs = []
-    for i, cmd in enumerate(cmds):
-        try:
-            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            procs.append((i, proc))
-        except Exception as e:
-            w( (i, (1, '', str(e))) )
-    return procs
+    def exec_commands(cmds):
+        procs = []
+        for i, cmd in enumerate(cmds):
+            try:
+                proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                procs.append((i, proc))
+            except Exception as e:
+                w( (i, (1, '', str(e))) )
+        return procs
 
-w("ready")
-commands = []
-signal.alarm(TIMEOUT)
-for line in iter(sys.stdin.readline, "done\n"):
-    commands.append(json.loads(line))
-procs = exec_commands(commands)
+    w("ready")
+    commands = []
+    signal.alarm(TIMEOUT)
+    for line in iter(sys.stdin.readline, "done\n"):
+        commands.append(json.loads(line))
+    procs = exec_commands(commands)
 
-cmd_mapping = {}
-fd_mapping = {}
-allfds = set()
-for i, proc in procs:
-    o = {"idx": i, "proc": proc, "stdout": [], "stderr": [], "waiting": 2}
-    fd_mapping[proc.stdout] = o["stdout"]
-    fd_mapping[proc.stderr] = o["stderr"]
-    cmd_mapping[proc.stdout] = o
-    cmd_mapping[proc.stderr] = o
-    allfds.update([proc.stderr, proc.stdout])
+    cmd_mapping = {}
+    fd_mapping = {}
+    allfds = set()
+    for i, proc in procs:
+        o = {"idx": i, "proc": proc, "stdout": [], "stderr": [], "waiting": 2}
+        fd_mapping[proc.stdout] = o["stdout"]
+        fd_mapping[proc.stderr] = o["stderr"]
+        cmd_mapping[proc.stdout] = o
+        cmd_mapping[proc.stderr] = o
+        allfds.update([proc.stderr, proc.stdout])
 
-while allfds:
-    r, _, _ = select.select(allfds, [], [])
-    for fd in r:
-        output = os.read(fd.fileno(), 1024)
-        if output:
-            fd_mapping[fd].append(output)
-            continue
+    while allfds:
+        r, _, _ = select.select(allfds, [], [])
+        for fd in r:
+            output = os.read(fd.fileno(), 1024)
+            if output:
+                fd_mapping[fd].append(output)
+                continue
 
-        cmd = cmd_mapping[fd]
-        cmd["waiting"] -= 1
-        if cmd["waiting"] != 0:
-            continue
+            cmd = cmd_mapping[fd]
+            cmd["waiting"] -= 1
+            if cmd["waiting"] != 0:
+                continue
 
-        proc = cmd["proc"]
-        res = proc.wait()
-        out = "".join(cmd["stdout"])
-        err = "".join(cmd["stderr"])
-        w( (cmd["idx"], (res, out, err)) )
-        allfds.remove(proc.stdout)
-        allfds.remove(proc.stderr)
+            proc = cmd["proc"]
+            res = proc.wait()
+            out = "".join(cmd["stdout"])
+            err = "".join(cmd["stderr"])
+            w( (cmd["idx"], (res, out, err)) )
+            allfds.remove(proc.stdout)
+            allfds.remove(proc.stderr)
 
-w("done")
-""".encode("zlib").encode("base64").replace("\n", "")
+    w("done")
+    """
+
+    muxer = textwrap.dedent(muxer)
+
+    return muxer.encode("zlib").encode("base64").replace("\n", "")
+
 
 CmdResult = collections.namedtuple("CmdResult", "status stdout stderr")
 
@@ -104,7 +111,7 @@ class SSHMaster:
 
     def send_commands(self, cmds, timeout=10):
         self.connect()
-        run_mux = """python -c 'exec("%s".decode("base64").decode("zlib"))'\n""" % muxer
+        run_mux = """python -c 'exec("%s".decode("base64").decode("zlib"))'\n""" % get_muxer()
         self.master.stdin.write(run_mux)
         self.master.stdin.flush()
         self.readline_with_timeout(timeout)
