08dfaa0a3f7896d4c8060f4d7368c1292c2225d8
Daniel Thayer <dnthayer@illinois.edu>
2015-10-26 13:56:50 -0500

    Change Bro default port number for broker communication
    This might help avoid conflicts or confusion with the old communication
    port numbers.

--- BroControl/options.py
+++ BroControl/options.py
@@ -45,7 +45,7 @@ options = [
            "The number of seconds to wait before assuming Broccoli communication events have timed out."),
     Option("CommandTimeout", 60, "int", Option.USER, False,
            "The number of seconds to wait for a command to return results."),
-    Option("BroPort", 47760, "int", Option.USER, False,
+    Option("BroPort", 9999, "int", Option.USER, False,
            "The TCP port number that Bro will listen on. For a cluster configuration, each node in the cluster will automatically be assigned a subsequent port to listen on."),
     Option("LogRotationInterval", 3600, "int", Option.USER, False,
            "The frequency of log rotation in seconds for the manager/standalone node (zero to disable rotation). This overrides the Bro script variable Log::default_rotation_interval."),
