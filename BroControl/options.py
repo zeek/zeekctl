2cc4e06411b4b434de3222d795e254c106e45d81
Jon Siwek <jsiwek@corelight.com>
2018-03-08 09:58:22 -0600

    Make Broker Control topic a config option

--- BroControl/options.py
+++ BroControl/options.py
@@ -37,6 +37,8 @@ options = [
            "The number of seconds to wait before sending a SIGKILL to a node which was previously issued the 'stop' command but did not terminate gracefully."),
     Option("CommTimeout", 10, "int", Option.USER, False,
            "The number of seconds to wait before assuming Broccoli communication events have timed out."),
+    Option("ControlTopic", "bro/control", "string", Option.USER, False,
+           "The Broker topic name used for sending and receiving control messages to Bro processes"),
     Option("CommandTimeout", 60, "int", Option.USER, False,
            "The number of seconds to wait for a command to return results."),
     Option("BroPort", 47760, "int", Option.USER, False,
