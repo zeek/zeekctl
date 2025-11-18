8d9a2900a0ae8b50bb3033031ac053ccc0bcd75c
Arne Welzel <arne.welzel@corelight.com>
2025-11-18 20:07:46 +0100

    cluster_backend_zeromq: Allocate XPUB/XSUB for proxy using ZeekPort
    This modifies the cluster_backend_zeromq plugin to read the ZeekPort
    option write back ZeekPort + 2, using up two ports for ZeroMQ.
    
    This causes a bunch of baseline diffs because the generated cluster
    layouts start a different ports now. This is a bit annoying because
    technically there's no reason for manager, worker and proxy nodes to
    have the $p member in the cluster-layout. At the same time, keep it
    there to make it easier to quickly switch back to Broker for testing
    without needing to re-generate the cluster-layout.
    
    We could have the cluster backend plugin propagate to install.py
    information about which nodes require port allocations, but I'm a
    bit conflicted if that is worth it (and see the argument for having
    ports populated for testing Broker above).

--- ZeekControl/plugins/cluster_backend_zeromq.py
+++ ZeekControl/plugins/cluster_backend_zeromq.py
@@ -12,6 +12,16 @@ class ClusterBackendZeroMQ(ZeekControl.plugin.Plugin):
     def pluginVersion(self):
         return 1
 
+    def options(self):
+        return [
+            (
+                "disable_unencrypted_warning",
+                "bool",
+                False,
+                "Disable the multi-node unencrypted warning.",
+            )
+        ]
+
     def init(self):
         """
         Enable the plugin if ClusterBackend setting is ZeroMQ.
@@ -24,6 +34,37 @@ class ClusterBackendZeroMQ(ZeekControl.plugin.Plugin):
         # the ZeroMQ cluster backend uses.
         if not self.getGlobalOption("ClusterTopicSeparator"):
             ZeekControl.config.Config.set_option("ClusterTopicSeparator", ".")
+
+        # Lookup ZeekPort from the config and increment it by two. Use these two
+        # ports to configure the XPUB/XSUB proxy component. The magic Port class
+        # is located over in install.py
+        base_port = ZeekControl.config.Config.get_option("ZeekPort")
+        self.xpub_port = base_port
+        self.xsub_port = base_port + 1
+        ZeekControl.config.Config.set_option("ZeekPort", base_port + 2)
+
+        # The manager address is used for listening of the XPUB/XSUB proxy.
+        self.xpub_xsub_addr = ZeekControl.config.Config.manager().addr
+
+        # Check if this is a multi-node cluster (multiple IP addresses) and
+        # tell the user about it.
+        addrs = {n.addr for n in self.nodes()}
+        if len(addrs) > 1 and not self.getOption("disable_unencrypted_warning"):
+            self.message(
+                f'Warning: ZeroMQ cluster backend enabled and multi-node cluster detected (IPs {", ".join(addrs)}).'
+            )
+            self.message(
+                "Communication between Zeek nodes using ZeroMQ is currently unencrypted. Use Broker with TLS if this"
+            )
+            self.message(
+                "is concerning to you. ZeroMQ encryption is tracked at https://github.com/zeek/zeek/issues/4432"
+            )
+            self.message(
+                "\nYou may disable this warning by setting the following option in zeekctl.cfg:"
+            )
+            self.message(
+                "\n    cluster_backend_zeromq.disable_unencrypted_warning = 1\n"
+            )
 
         return True
 
@@ -39,6 +80,11 @@ class ClusterBackendZeroMQ(ZeekControl.plugin.Plugin):
                 "@else",
                 "@load policy/frameworks/cluster/backend/zeromq/connect",
                 "@endif",
+                "",
+                f'redef Cluster::Backend::ZeroMQ::listen_xpub_endpoint = "tcp://{self.xpub_xsub_addr}:{self.xpub_port}";',
+                f'redef Cluster::Backend::ZeroMQ::listen_xsub_endpoint = "tcp://{self.xpub_xsub_addr}:{self.xsub_port}";',
+                f'redef Cluster::Backend::ZeroMQ::connect_xpub_endpoint = "tcp://{self.xpub_xsub_addr}:{self.xsub_port}";',
+                f'redef Cluster::Backend::ZeroMQ::connect_xsub_endpoint = "tcp://{self.xpub_xsub_addr}:{self.xpub_port}";',
                 "",
             ]
         )
